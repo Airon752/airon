c35b6318cfa2873f5b49ede47c5f48775cb93773
mindlessbrain <drodriguesdelima@gmail.com>
2017-09-15 23:28:16 -0300

    Código refatorado

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# airon
+# Consulta a Previsão do Tempo com Python
+
+Consulte a previsão do tempo para sua cidade no site [tempoagora.com.br](http://www.tempoagora.com.br/previsao-do-tempo).
+
+#### Instale as dependências com pip
+```pip install -r requirements.txt```
+
+### Exemplo de uso:
+```python3 tempo.py```
+```
+Consultando a previsão do tempo em tempoagora.com.br
+Entre com os dados para
+Estado: sp
+Cidade: São Paulo
+
+
+Previsão do Tempo para São Paulo - SP - Tempo Agora
+Temperatura:  18°C
+Sesação:  17°C
+Velocidade do Vento:  20.37 km/h
+Pressão:  1026 mb
+Umidade: 82.65%
+```
--- tempo.py
+++ tempo.py
@@ -1,25 +1,69 @@
 import requests
+import json
+import os
 from bs4 import BeautifulSoup
+from unicodedata import normalize
+
+
+with open('states.json') as file:
+    estados = json.load(file)
+
+
+def removeAcentos(txt):
+    return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
+
+
+def getEstado():
+    txt = input("Estado: ").replace(" ", "").lower()
+    estado = removeAcentos(txt)
+    return estados[estado] if estado in estados else None
+
+
+def getCidade():
+    txt = input("Cidade: ").replace(" ", "").lower()
+    return removeAcentos(txt)
+
+
+def printTempo(soup):
+    title = soup.find_all("title")[0].get_text()
+    if title != 'Previsão do Tempo para - - Tempo Agora':
+        print('\n')
+        print(title)
+        print("Temperatura: ", soup.find_all("span")[5].get_text())
+        print("Sesação: ", soup.find_all("span")[7].get_text())
+        print("Velocidade do Vento: ", soup.find_all("span")[9].get_text())
+        print("Pressão: ", soup.find_all("span")[11].get_text())
+        print("Umidade: "+soup.find_all("span")[13].get_text())
+
+    else:
+        print("Cidade não encontrada")  # o estado já foi checado
+
 
 def tempo():
+    url_base = "http://www.tempoagora.com.br/previsao-do-tempo"
+    while True:
+        os.system('clear')
 
-    site = requests.get("http://www.tempoagora.com.br/previsao-do-tempo"+"/"+str(input("Digite a sigla do estado onde se localiza a cidade que você que consulta o tempo: "))+"/"+str(input("Digite o nome da cidade: "))+"/")
-    soup = BeautifulSoup(site.content, 'html.parser')
+        print("Consultando a previsão do tempo em tempoagora.com.br")
+        print("Entre com os dados para")
 
-    titulo = soup.find_all("title")[0].get_text()
-    temperatura = soup.find_all("span")[5].get_text()
-    sensacao = soup.find_all("span")[7].get_text()
-    velocidade = soup.find_all("span")[9].get_text()
-    pressao = soup.find_all("span")[11].get_text()
-    umidade = soup.find_all("span")[13].get_text()
+        estado = getEstado()
 
-    print(titulo)
-    print("Temperatura: "+temperatura)
-    print("Sesação: "+sensacao)
-    print("Velocidade do Vento: "+velocidade)
-    print("Pressão: "+pressao)
-    print("Umidade: "+umidade)
+        if estado:
+            cidade = getCidade()
+        else:
+            print("Estado não encontrado\n\n\n")
 
-tempo()
+        url = "{}/{}/{}/".format(url_base, estado, cidade)
+        site = requests.get(url)
+        printTempo(BeautifulSoup(site.content, 'html.parser'))
 
-print(str(input("pressione Enter para SAIR")))
+        continuar = ''
+        while continuar != 's' and continuar != 'n':
+            continuar = input("Deseja continuar pesquisando? [S/n]:  ").lower()
+        if continuar == 'n':
+            return
+
+
+if __name__ == '__main__':
+    tempo()
